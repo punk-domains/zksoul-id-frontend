5d57b7b31f3c40a2501f3475383d340e0e2a146d
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-04-19 12:39:04 +0200

    about text added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,64 @@
 <template>
 <div class="container">
-  <h1 class="text-center">About ID</h1>
+  <h1 class="text-center mt-3">About zkSoul ID</h1>
   
-  <div class="row mt-3">
+  <div class="row mt-5">
     <div class="col-md-8 offset-md-2">
       <p>
-        zkSoul ID is the first soulbound identity on the zkSync network. Soulbound means that it cannot be transferred to another address 
-        once it is minted.
+        zkSoul ID is a groundbreaking concept that introduces the idea of soulbound identities on zkSync.
+      </p>
+
+      <h3>Free mint</h3>
+
+      <p>
+        The first zkSoul ID for an address is free, but only for IDs that are 5+ characters long. This incentivizes users to 
+        adopt zkSoul ID as their primary mode of identification on zkSync.
+      </p>
+
+      <h3>ID = Wallet Address</h3>
+
+      <p>
+        Using zkSoul ID as a means of identification makes it easier to send and receive tokens.
+      </p>
+
+      <p>
+        Since the zkSoul ID is bound to 
+        your address, you can send tokens to any zkSoul ID or receive tokens to your zkSoul ID without having to remember the other 
+        person's address.
+      </p>
+
+      <p>
+        It's much easier to remember a zkSoul ID than a crypto address.
+      </p>
+
+      <h3>Bound</h3>
+
+      <p>
+        Soulbound identity is a non-transferable NFT bound to an address, so the address behind an ID always stays the same.
+      </p>
+
+      <p>
+        This way, you don't have to worry about sending tokens to the wrong address or losing your ID.
+      </p>
+
+      <h3>Web3 Social ID</h3>
+
+      <p>
+        The major use case for zkSoul IDs is to use them as usernames in the upcoming Web3 Social world.
+      </p>
+
+      <p>
+        This is why we are building a decentralized social network called zkChat where zkSoul IDs will be used as usernames.
+      </p>
+
+      <h3>Join Us</h3>
+
+      <p>
+        We believe that zkSoul ID has the potential to revolutionize the way we interact with each other in the web3 world.
+      </p>
+
+      <p>
+        Join us on this exciting journey and become a part of the future of decentralized social networking!
       </p>
 
     </div>
